7f14c1e958390e9c5b3f2e1719beb504e42ceb54
Vova Kostenko <vova.kostenko.2007@inbox.ru>
2024-07-23 17:47:15 +0300

    First commit

---
+++ Player.h
@@ -0,0 +1,98 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#pragma once
+class Player
+{
+private:
+	
+
+public:
+	int i_place = 5, j_place = 5, k = 0, spd = 800;
+	bool game = true;
+	int apple_place[2];
+	std::vector <std::vector <int>> body;
+
+	Player(std::vector <std::vector <int>> body = { {5, 5}, {6, 5} }) {
+		this->body = body;
+	}
+
+	void LvlUp() {			//Поднятие уровня змейки (увеличение ее размера)
+		spd -= 25;
+		body.push_back({ body[body.size() - 1][0], body[body.size() - 1][1] });
+	}
+
+	void Apple_Spawn() {			//Случайный спавн яблока
+		int x = rand() % (9 - 0 + 1);
+		int y = rand() % (9 - 0 + 1);
+		apple_place[0] = x;
+		apple_place[1] = y;
+
+	}
+
+	void Moving() {				//Передвижение змейки
+		while (game)
+		{
+			if (i_place == 10 || j_place == 10 || i_place == -1 || j_place == -1) {
+				break;
+				game = false;
+			}			
+			if (k == 1) {
+				i_place--;
+				for (int i = body.size() - 1; i > 0; --i) {
+					for (int j = 0; j < 2; ++j) {
+						if (body[i][0] == i_place && body[i][1] == j_place) {
+							game = false;
+						}
+						body[i][j] = body[i - 1][j];
+					}
+				}
+				body[0] = { i_place, j_place };
+				std::this_thread::sleep_for(std::chrono::milliseconds(spd));
+			}
+			else if (k == 2) {
+				i_place++;
+				for (int i = body.size() - 1; i > 0; --i) {
+					for (int j = 0; j < 2; ++j) {
+						if (body[i][0] == i_place && body[i][1] == j_place) {
+							game = false;
+						}
+						body[i][j] = body[i - 1][j];
+					}
+				}
+				body[0] = { i_place, j_place };
+				std::this_thread::sleep_for(std::chrono::milliseconds(spd));
+			}
+			else if (k == 3){
+				j_place--;
+				for (int i = body.size() - 1; i > 0; --i) {
+					for (int j = 0; j < 2; ++j) {
+						if (body[i][0] == i_place && body[i][1] == j_place) {
+							game = false;
+						}
+						body[i][j] = body[i - 1][j];
+					}
+				}
+				body[0] = { i_place, j_place };
+				std::this_thread::sleep_for(std::chrono::milliseconds(spd));				
+			}
+			else if (k == 4) {
+				j_place++;
+				for (int i = body.size() - 1; i > 0; --i) {
+					for (int j = 0; j < 2; ++j) {
+						if (body[i][0] == i_place && body[i][1] == j_place) {
+							game = false;
+						}
+						body[i][j] = body[i - 1][j];
+					}
+				}
+				body[0] = { i_place, j_place };
+				std::this_thread::sleep_for(std::chrono::milliseconds(spd));
+			}			
+		}
+	}
+};
+
---
+++ Snake.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <vector>
+#include <conio.h>
+#include <thread>
+#include <chrono>
+#include "Player.h"
+
+
+Player snake;
+
+void key_detect() {             //Функция определения нажатой клавиши
+	while (snake.game)
+	{
+		if (snake.i_place == 10 || snake.j_place == 10) {
+			break;
+		}
+		char key = _getch();
+		if (key == 0x48) {
+			snake.k = 1;
+		}
+		else if (key == 0x50) {
+			snake.k = 2;
+		}
+		else if (key == 0x4B) {
+			snake.k = 3;
+		}
+		else if (key == 0x4D) {
+			snake.k = 4;
+		}
+	}
+}
+
+void Map_Draw() {							//Отрисовка карты
+	std::vector <std::vector <char>> map(10, std::vector <char>(10, '.'));
+	snake.Apple_Spawn();
+	
+	while (snake.game) {		
+		if (snake.i_place == 10 || snake.j_place == 10) {
+			break;
+		}
+		std::vector <std::vector <char>> map(10, std::vector <char>(10, '.'));
+		map[snake.apple_place[0]][snake.apple_place[1]] = '0';
+		if (snake.i_place == 10 || snake.j_place == 10) {
+			snake.game = false;
+		}
+
+		for (int i = 0; i < snake.body.size() - 1; ++i) {
+			map[snake.body[i][0]][snake.body[i][1]] = '8';
+		}
+		
+
+		if (map[snake.i_place][snake.j_place] == map[snake.apple_place[0]][snake.apple_place[1]]) {
+			snake.LvlUp();
+			snake.Apple_Spawn();
+			map[snake.apple_place[0]][snake.apple_place[1]] = '0';
+		}
+
+
+		for (size_t i = 0; i < 10; ++i) {
+			for (size_t j = 0; j < 10; ++j) {
+				std::cout << map[i][j] << ' ';
+			}
+			std::cout << '\n';
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds (snake.spd));
+		system("cls");
+		
+	}
+}
+
+int main() {
+	std::thread thr(key_detect);                //Создание отдельных потоков для функций
+	std::thread draw(Map_Draw);
+
+	snake.Moving();
+
+	thr.detach();
+	draw.detach();
+
+	std::cout << "Game over";
+	return 0;
+}
